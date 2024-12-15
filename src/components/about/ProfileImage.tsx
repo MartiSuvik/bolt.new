20a0abf9a4c80211e7b36d76628143a31c668575
Effi <77239232+MartiSuvik@users.noreply.github.com>
2024-12-16 00:09:16 +0200

    Update ProfileImage.tsx

--- src/components/about/ProfileImage.tsx
+++ src/components/about/ProfileImage.tsx
@@ -3,8 +3,8 @@ export default function ProfileImage() {
     <div className="container-inner">
       <img
         className="profile-img"
-        src="https://i.ibb.co/VxKN3Mg/martin.png"
-        alt="Martin - Founder"
+        src="https://drive.google.com/u/0/drive-viewer/AKGpihZCWrpUoPMCsw79GR3aPsaeB75UuQmSgQ107R6_AiKsLmdUaa8gcnl0XOqfyM8CzOwPT0rhsiElCEMnyZ1W_HrL4DxSqBsQGbw=s1600-rw-v1"
+        alt="Marti - Founder"
       />
     </div>
   );
